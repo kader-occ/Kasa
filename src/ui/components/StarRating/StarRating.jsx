6e0a83b71fdd0edc323f46bce6d0495e9e7e4502
dev_cook <studiokad75@gmail.com>
2024-06-20 22:13:38 +0200

    feature/app/update

--- src/ui/components/StarRating/StarRating.jsx
+++ src/ui/components/StarRating/StarRating.jsx
@@ -4,15 +4,16 @@ import "./StarRating.scss";
 const StarRating = ({ rating }) => {
   const starPercentage = (rating / 5) * 100;
 
-  const starStyle = {
-    width: `${starPercentage}%`,
-    color: "#FF6060",
-  };
-
   return (
     <div className="star-rating">
       <div className="stars-outer">
-        <div className="stars-inner" style={starStyle}></div>
+        <div className="stars-inner" style={{ width: `${starPercentage}%` }}>
+          {[...Array(5)].map((_, index) => (
+            <span key={index} className="star">
+              &#9733;
+            </span>
+          ))}
+        </div>
       </div>
       <span className="number-rating">{rating} / 5</span>
     </div>
