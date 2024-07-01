8766d38bc84632959006f3b79d111bc3d005121f
dev_cook <studiokad75@gmail.com>
2024-07-01 20:02:52 +0200

    feature/app/update

--- src/ui/components/ImageSlider/ImageSlider.jsx
+++ src/ui/components/ImageSlider/ImageSlider.jsx
@@ -34,6 +34,9 @@ const ImageSlider = ({ images }) => {
           &#10095;
         </button>
       )}
+      <p className="image-counter">
+        {currentIndex + 1} / {images.length}
+      </p>
     </div>
   );
 };
